d081dbf4e65936a8277abc4286703a5839298382
kalz2q <katsu95i@gmail.com>
2022-09-07 14:28:57 +0900

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="cat">
+  <div className="cat__ear"></div>
+  <div className="cat__face">
+    <div className="cat__eye"></div>
+    <div className="cat__beard"></div>
+    <div className="cat__mouth"></div>
+  </div>
+  <div className="cat__body">
+    <div className="cat__hand">
+      <div className="cat__hand__l"></div>
+      <div className="cat__hand__r"></div>
+    </div>
+  </div>
+  <div className="cat__foot">
+    <div className="cat__foot__l"></div>
+    <div className="cat__foot__r"></div>
+  </div>
+  <div className="cat__tail">
+    <div className="cat__tail__l"></div>
+    <div className="cat__tail__r"></div>
+  </div>
+</div>
+
+
+
+
+
     </div>
   );
 }
